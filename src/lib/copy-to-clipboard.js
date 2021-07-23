0b6734ff3eaada2a50e1e612265cfbeaa24e45b8
pbca26 <pbca26@gmail.com>
2021-07-23 19:49:23 +0300

    add copy to clipboard helper

---
+++ src/lib/copy-to-clipboard.js
@@ -0,0 +1,18 @@
+const copyToClipboard = (value) => {
+  const copyTextarea = document.querySelector('#js-copytextarea');
+  let result;
+  
+  copyTextarea.value = value;
+  copyTextarea.select();
+
+  try {
+    document.execCommand('copy');
+    result = 1;
+  } catch (err) {
+    result = 0;
+  }
+
+  return result;
+};
+
+export default copyToClipboard;
