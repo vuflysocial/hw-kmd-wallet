ff6c9057b6ac1d6e8ceb110413b67abc36d85787
pbca26 <pbca26@gmail.com>
2021-08-24 15:25:45 +0300

    time helpers add secondsElapsedToString method

--- src/lib/time.js
+++ src/lib/time.js
@@ -33,7 +33,54 @@ const checkTimestamp = (dateToCheck, currentEpochTime = Date.now() / 1000) => {
   return Math.floor(secondsElapsed);
 };
 
+// src: https://stackoverflow.com/questions/8942895/convert-a-number-of-days-to-days-months-and-years-with-jquery/8943500
+const secondsElapsedToString = (timestamp, srcInSeconds) => { // in seconds
+  let secondsElapsed = srcInSeconds ? timestamp : checkTimestamp(timestamp);
+  let str = '';
+  // Map lengths of `secondsElapsed` to different time periods
+  const oneDay = 24 * 3600;
+  const values = [{
+    str: ' year',
+    num: 365 * oneDay,
+  }, {
+    str: ' month',
+    num: 30 * oneDay,
+  }, {
+    str: ' week',
+    num: 7 * oneDay,
+  }, {
+    str: ' day',
+    num: 1 * oneDay,
+  }, {
+    str: ' hour',
+    num: 3600,
+  }, {
+    str: ' minute',
+    num: 60,
+  }, {
+    str: ' second',
+    num: 1,
+  }];
+
+  // Iterate over the values...
+  for (let i = 0; i < values.length; i++) {
+    const _value = Math.floor(secondsElapsed / values[i].num);
+
+    // ... and find the largest time value that fits into the secondsElapsed
+    if (_value >= 1) {
+      // If we match, add to the string ('s' is for pluralization)
+      str += `${_value + values[i].str + (_value > 1 ? 's' : '')} `;
+
+      // and subtract from the secondsElapsed
+      secondsElapsed -= _value * values[i].num;
+    }
+  }
+
+  return str.trim(); // trim space chars
+};
+
 module.exports = {
   secondsToString,
-  checkTimestamp
+  checkTimestamp,
+  secondsElapsedToString,
 };
