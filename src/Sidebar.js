b1b24f84be8375b458def443dd8945e543b5ae79
pbca26 <pbca26@gmail.com>
2021-09-20 13:44:41 +0300

    update sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import AboutModal from './AboutModal';
+import SendCoinButton from './SendCoinButton';
+import ReceiveCoinButton from './ReceiveCoinButton';
+import SettingsModal from './SettingsModal';
 
 class Sidebar extends React.Component {
   state = this.initialState;
@@ -10,11 +13,43 @@ class Sidebar extends React.Component {
   };
 
   render() {
-    console.warn(this.props);
+    console.warn('sidebar props', this.props);
     
     return (
       <div className="sidebar-right">
         <ul>
+          {this.props.isCoinData() &&
+           this.props.activeCoin &&
+           this.props.vendor &&
+            <li onClick={() => this.props.setActiveCoin(null)}>
+              <i className="fa fa-coins"></i>
+            </li>
+          }
+          {this.props.activeAccount !== null &&
+            <li onClick={() => this.props.setActiveAccount(null)}>
+              <i className="fa fa-wallet"></i>
+            </li>
+          }
+          {this.props.activeCoin &&
+            <SendCoinButton
+              accounts={this.props.accounts}
+              handleRewardClaim={this.props.handleRewardClaim}
+              vendor={this.props.vendor}
+              syncData={this.props.syncData}
+              coin={this.props.activeCoin}>
+              Send
+            </SendCoinButton>
+          }
+          {this.props.activeCoin &&
+            <ReceiveCoinButton
+              vendor={this.props.vendor}
+              coin={this.props.activeCoin}
+              accounts={this.props.accounts}>
+              Receive
+            </ReceiveCoinButton>
+          }
+          <SettingsModal
+            coin={this.props.activeCoin}/>
           <li>
             <a href="https://github.com/pbca26/hw-kmd-wallet/issues/new"><i className="fa fa-life-ring"></i></a>
           </li>
