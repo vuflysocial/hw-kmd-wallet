e2c1a16ce5f65f4a9d08ad6f6a5a78ce8ca8a41a
pbca26 <pbca26@gmail.com>
2021-10-20 16:58:30 +0300

    (tests) add async test

---
+++ src/test/lib.async.spec.js
@@ -0,0 +1,21 @@
+import asyncForEach from '../lib/async';
+
+const remoteCall = (a) => {
+  return new Promise((resolve, reject) => {
+    resolve(a + 1);
+  });
+};
+
+const fn = async (a) => {
+  let res = 0;
+
+  await asyncForEach(a, async (item, index) => {
+    res += await remoteCall(item);
+  });
+
+  return res;
+};
+
+test('it should run async series on array of elements and return 9', async () => {
+  expect(await fn([1, 2, 3])).toBe(9);
+});
