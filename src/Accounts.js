4aed058ae2f771299114b56f56455bbef40a2811
pbca26 <pbca26@gmail.com>
2021-11-11 22:49:40 +0300

    accounts comp get debug val from settings

--- src/Accounts.js
+++ src/Accounts.js
@@ -14,6 +14,7 @@ import {
   appData,
 } from './Electron';
 import {writeLog} from './Debug';
+import {getLocalStorageVar} from './lib/localstorage-util';
 
 // TODO: add a way to remove/add accounts
 
@@ -31,7 +32,7 @@ class Account extends React.Component {
       sendTo: '',
       // debug options
       showXpub: null,
-      isDebug: isElectron ? appData.isDev : window.location.href.indexOf('enable-verify') > -1,
+      isDebug: isElectron ? appData.isDev : window.location.href.indexOf('enable-verify') > -1 || getLocalStorageVar('settings') && getLocalStorageVar('settings').enableDebugTools,
     };
   }
 
