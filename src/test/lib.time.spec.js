c5a888f7c6f5f7688183dcb367719d2d57d06517
pbca26 <pbca26@gmail.com>
2021-11-21 17:45:11 +0300

    (tests) fix time test error

--- src/test/lib.time.spec.js
+++ src/test/lib.time.spec.js
@@ -15,12 +15,12 @@ test('it should return 60 (time - checkTimestamp 60s)', () => {
   expect(checkTimestamp(timeExample, timeExample2 / 1000)).toBe(60);
 });
 
-test('it should return 23 Feb 2018 03:00 (time - secondsToString)', () => {
+test('it should return 23 Feb 2018 00:00 (time - secondsToString)', () => {
   expect(secondsToString(timeExample / 1000)).toBe('23 Feb 2018 03:00');
 });
 
-test('it should return 23 Feb 2018 03:00:0 (time - secondsToString)', () => {
-  expect(secondsToString(timeExample / 1000, false, true)).toBe('23 Feb 2018 03:00:0');
+test('it should return 23 Feb 2018 00:00:0 (time - secondsToString)', () => {
+  expect(secondsToString(timeExample / 1000, false, true)).toBe('23 Feb 2018 00:00:0');
 });
 
 test('it should return 1 second (time - secondsElapsedToString - 1s (singular))', () => {
