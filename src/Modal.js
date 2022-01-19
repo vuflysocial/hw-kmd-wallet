f432279fb6edc6ae5afa8d752b61baf32a215329
pbca26 <pbca26@gmail.com>
2022-01-19 15:17:48 +0300

    #260 fix overscroll issue when modal is active

--- src/Modal.js
+++ src/Modal.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import './Modal.scss';
 
-const Modal = ({
-  children,
-  title,
-  show,
-  isCloseable,
-  handleClose,
-  className
-}) => (
-  <div className={`Modal modal ${show ? 'is-active' : ''}${className ? ' ' + className : ''}`}>
-    <div onClick={() => isCloseable && handleClose && handleClose()}>
-      <div className="modal-background"></div>
-      <button className={`modal-close is-large ${!isCloseable ? 'is-invisible' : ''}`}></button>
-    </div>
-    <div className="modal-content">
-      <div className="card">
-        <header className="card-header">
-          <p className="card-header-title">
-            {title}
-          </p>
-        </header>
-        <div className="card-content">
-          <div className="content">
-            {children}
+class Modal extends React.Component {
+  componentDidUpdate() {
+    const modals = document.getElementsByClassName('Modal modal is-active');
+    const docroot = document.getElementsByTagName('html')[0];
+    
+    docroot.className = modals.length ? 'no-scroll' : 'scroll';
+  }
+
+  render() {
+    const {
+      children,
+      title,
+      show,
+      isCloseable,
+      handleClose,
+      className
+    } = this.props;
+
+    return (
+      <div className={`Modal modal ${show ? 'is-active' : ''}${className ? ' ' + className : ''}`}>
+        <div onClick={() => isCloseable && handleClose && handleClose()}>
+          <div className="modal-background"></div>
+          <button className={`modal-close is-large ${!isCloseable ? 'is-invisible' : ''}`}></button>
+        </div>
+        <div className="modal-content">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                {title}
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                {children}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 export default Modal;
