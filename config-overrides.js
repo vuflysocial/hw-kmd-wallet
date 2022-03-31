5c46289d2310fff3e318782d3e57a871e8b1d5a3
pbca26 <pbca26@gmail.com>
2022-03-31 13:02:39 +0300

    update config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,39 +1,23 @@
-const hotLoader = require('react-app-rewire-hot-loader');
-const rewireSass = require('react-app-rewire-scss');
+const webpack = require('webpack');
 const {paths} = require('react-app-rewired');
 const path = require('path');
 
-module.exports = {
-  webpack: function (config, env) {
-    // workaround for es6 node_modules minify errors
-    config.module.rules[1].oneOf[1].include = [
-      paths.appSrc,
-      path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
-      path.resolve(paths.appNodeModules, 'ow'),
-      path.resolve(paths.appNodeModules, 'create-xpub'),
-      path.resolve(paths.appNodeModules, 'build-output-script'),
-      path.resolve(paths.appNodeModules, 'get-komodo-rewards'),
-      path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
-      path.resolve(paths.appNodeModules, 'bip32'),
-      path.resolve(paths.appNodeModules, 'typeforce'),
-      path.resolve(paths.appNodeModules, 'semver'),
-      path.resolve(paths.appNodeModules, 'lru-cache'),
-      path.resolve(paths.appNodeModules, 'yallist'),
-      path.resolve(paths.appNodeModules, 'u2f-api'),
-      path.resolve(paths.appNodeModules, 'asn1.js'),
-      path.resolve(paths.appNodeModules, 'bech32'),
-      path.resolve(paths.appNodeModules, 'webrtc-adapter'),
-      path.resolve(paths.appNodeModules, '@trezor/utxo-lib'),
-      path.resolve(paths.appNodeModules, '@ledgerhq/hw-app-btc'),
-      path.resolve(paths.appNodeModules, '@ledgerhq/hw-transport/lib-es'),
-      path.resolve(paths.appNodeModules, '@ledgerhq/hw-transport-webusb/lib-es'),
-      path.resolve(paths.appNodeModules, '@ledgerhq/hw-transport-u2f/lib-es'),
-      path.resolve(paths.appNodeModules, '@ledgerhq/hw-transport-webhid/lib-es'),
-    ];
+module.exports = function override(config) {
+  config.ignoreWarnings = [/Failed to parse source map/];
+  
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    'crypto': require.resolve('crypto-browserify'),
+    'buffer': require.resolve('buffer'),
+    'stream': require.resolve('stream-browserify'),
+  })
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]);
 
-    config = hotLoader(config, env);
-    config = rewireSass(config, env);
-
-    return config;
-  }
+  return config;
 }
