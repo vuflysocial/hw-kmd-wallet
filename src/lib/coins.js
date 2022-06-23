60f2715bebe7e93117cb3d8b0ee8653a5d057893
pbca26 <pbca26@gmail.com>
2022-06-23 13:07:50 +0300

    #316 disable AXO, CCL, WLC21, COQUICASH, BTCH, WSB

--- src/lib/coins.js
+++ src/lib/coins.js
@@ -1,5 +1,3 @@
-import {sortObject} from './sort';
-
 const coins = {
   KMD: {
     explorer: 'https://kmd.explorer.dexstats.info/',
@@ -10,94 +8,117 @@ const coins = {
     prices: {
       coinpaprika: 'kmd-komodo',
     },
+    enabled: true,
   },
   AXO: {
     explorer: 'https://axo.explorer.dexstats.info/',
     api: ['https://axo.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: false,
   },
   KOIN: {
     explorer: 'https://koin.explorer.dexstats.info/',
     api: ['https://koin.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   MESH: {
     explorer: 'https://mesh.explorer.dexstats.info/',
     api: ['https://mesh.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   DEX: {
     explorer: 'https://dex.explorer.dexstats.info/',
     api: ['https://dex.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   SUPERNET: {
     explorer: 'https://supernet.explorer.dexstats.info/',
     api: ['https://supernet.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   CCL: {
     explorer: 'https://ccl.explorer.dexstats.info/',
     api: ['https://ccl.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: false,
   },
   PGT: {
     explorer: 'https://pgt.explorer.dexstats.info/',
     api: ['https://pgt.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   MSHARK: {
     explorer: 'https://mshark.explorer.dexstats.info/',
     api: ['https://mshark.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   REVS: {
     explorer: 'https://revs.explorer.dexstats.info/',
     api: ['https://revs.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   PANGEA: {
     explorer: 'https://pangea.explorer.dexstats.info/',
     api: ['https://pangea.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   JUMBLR: {
     explorer: 'https://jumblr.explorer.dexstats.info/',
     api: ['https://jumblr.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   BET: {
     explorer: 'https://bet.explorer.dexstats.info/',
     api: ['https://bet.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   CRYPTO: {
     explorer: 'https://crypto.explorer.dexstats.info/',
     api: ['https://crypto.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   HODL: {
     explorer: 'https://hodl.explorer.dexstats.info/',
     api: ['https://hodl.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   ILN: {
     explorer: 'https://explorer.ilien.io/',
     api: ['https://explorer.ilien.io/insight-api-komodo/'],
+    enabled: true,
   },
   BOTS: {
     explorer: 'https://bots.explorer.dexstats.info/',
     api: ['https://bots.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   MGW: {
     explorer: 'https://mgw.explorer.dexstats.info/',
     api: ['https://mgw.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   WLC21: {
     explorer: 'https://wlc21.explorer.dexstats.info/',
     api: ['https://wlc21.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: false,
   },
   COQUICASH: {
     explorer: 'https://coquicash.explorer.dexstats.info/',
     api: ['https://coquicash.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: false,
   },
   BTCH: {
     explorer: 'https://btch.explorer.dexstats.info/',
     api: ['https://btch.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: false,
   },
   NINJA: {
     explorer: 'https://ninja.explorer.dexstats.info/',
     api: ['https://ninja.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   THC: {
     explorer: 'https://thc.explorer.dexstats.info/',
     api: ['https://thc.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   MCL: {
     explorer: 'https://mcl.explorer.dexstats.info/',
@@ -105,10 +126,12 @@ const coins = {
     prices: {
       coinpaprika: 'mcl-marmara-credit-loops',
     },
+    enabled: true,
   },
   LABS: {
     explorer: 'https://labs.explorer.dexstats.info/',
     api: ['https://labs.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   VOTE2022: {
     explorer: 'https://vote.explorer.dexstats.info/',
@@ -118,6 +141,7 @@ const coins = {
       'https://vote.kmdexplorer.io/insight-api-komodo/',
     ],
     airdrop: true,
+    enabled: true,
   },
   RICK: {
     explorer: 'https://rick.explorer.dexstats.info/',
@@ -126,6 +150,7 @@ const coins = {
       'https://rick.explorer.dexstats.info/insight-api-komodo/',
       'https://rick.kmd.dev/insight-api-komodo/',
     ],
+    enabled: true,
   },
   MORTY: {
     explorer: 'https://morty.explorer.dexstats.info/',
@@ -134,6 +159,7 @@ const coins = {
       'https://explorer.komodoplatform.com:10000/morty/api/',
       'https://morty.kmd.dev/insight-api-komodo/',
     ],
+    enabled: true,
   },
   VRSC: {
     explorer: 'https://vrsc.explorer.dexstats.info/',
@@ -145,6 +171,7 @@ const coins = {
     prices: {
       coinpaprika: 'vrsc-verus-coin',
     },
+    enabled: true,
   },
   WSB: {
     explorer: 'https://wsb.explorer.dexstats.info/',
@@ -153,10 +180,12 @@ const coins = {
       'https://explorer.komodoplatform.com:10000/wsb/api/',
     ],
     airdrop: true,
+    enabled: false,
   },
   SPACE: {
-    explorer: 'https://space.explorer.dexstats.info/',
-    api: ['https://space.explorer.dexstats.info/insight-api-komodo/'],
+    explorer: 'https://explorer.spaceworks.co/',
+    api: ['https://explorer.spaceworks.co/api/'],
+    enabled: true,
   },
   CLC: {
     explorer: 'https://clc.explorer.dexstats.info/',
@@ -164,14 +193,17 @@ const coins = {
     prices: {
       coinpaprika: 'clc-collider-coin',
     },
+    enabled: true,
   },
   SOULJA: {
     explorer: 'https://soulja.explorer.dexstats.info/',
     api: ['https://soulja.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   DP: {
     explorer: 'https://dp.explorer.dexstats.info/',
     api: ['https://dp.explorer.dexstats.info/insight-api-komodo/'],
+    enabled: true,
   },
   GLEEC: {
     explorer: 'https://gleec.xyz/',
@@ -182,6 +214,7 @@ const coins = {
     prices: {
       coinpaprika: 'gleec-gleec-coin',
     },
+    enabled: true,
   },
   TOKEL: {
     explorer: 'https://tokel.explorer.dexstats.info/',
@@ -193,6 +226,7 @@ const coins = {
     prices: {
       coinpaprika: 'tkl-tokel',
     },
+    enabled: true,
   },
   TKLTEST: {
     explorer: 'https://tkltest.explorer.dexstats.info/',
@@ -200,6 +234,7 @@ const coins = {
       'https://tkltest.explorer.dexstats.info/insight-api-komodo/',
       'https://explorer.komodoplatform.com:10000/tkltest/api/',
     ],
+    enabled: true,
   },
   /* coins below need special handling due to no overwinter support
   ZILLA: {
@@ -213,4 +248,4 @@ const coins = {
   */
 };
 
-export default sortObject(coins);
+module.exports = coins;
